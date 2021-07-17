c09e2aed2c63d751ec1c3416201aca36dd47b050
Aditya Tiwari <aditya.tiwari@nextsteps.ai>
2021-07-17 21:50:07 +0530

    add Layout page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,3 +1,11 @@
+import { useRouter } from 'next/dist/client/router';
+import Layout from '../../components/Layout';
 export default function EventPage() {
-	return <div>Event Page</div>;
+	const router = useRouter();
+	console.log(router);
+	return (
+		<Layout>
+			<h1>Event Page</h1>
+		</Layout>
+	);
 }
